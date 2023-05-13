727335774a1f8d1bc119998ac4155bd7b8910ec8
Wdson Danrlei <wdson91@gmail.com>
2023-05-13 18:41:17 -0300

    Projeto Inicial

--- src/main/java/com/api/parkingcontrol/configs/DateConfig.java
+++ src/main/java/com/api/parkingcontrol/configs/DateConfig.java
@@ -12,7 +12,7 @@ import java.time.format.DateTimeFormatter;
 @Configuration
 public class DateConfig {
     public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
-    public static LocalDateTimeSerializer LOCAL_DATETIME_SERIALIZER = new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
+    public static  LocalDateTimeSerializer LOCAL_DATETIME_SERIALIZER = new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
 
     @Bean
     @Primary
